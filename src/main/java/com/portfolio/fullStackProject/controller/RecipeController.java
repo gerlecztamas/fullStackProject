6c8aa513863333158dc5be1c339696ffee511d38
gerlecztamas <gerlecztamas@gmail.com>
2023-08-13 09:52:33 +0200

    implementing very simple logging using log4j2

--- src/main/java/com/portfolio/fullStackProject/controller/RecipeController.java
+++ src/main/java/com/portfolio/fullStackProject/controller/RecipeController.java
@@ -3,6 +3,8 @@ package com.portfolio.fullStackProject.controller;
 import com.portfolio.fullStackProject.entity.Recipe;
 import com.portfolio.fullStackProject.service.RecipeService;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,58 +16,71 @@ import java.util.List;
 @RequestMapping("")
 public class RecipeController {
     private final RecipeService recipeService;
+
+    private static final Logger LOGGER = LogManager.getLogger(RecipeController.class);
+
     @RequestMapping(value = "/getRecipes", method = RequestMethod.GET)
     public ResponseEntity<?> getRecipes(){
         try{
+            LOGGER.info("getRecipes method called in controller");
             List<Recipe> donations = recipeService.getRecipes();
             return new ResponseEntity<>(donations, HttpStatus.OK);
         }
         catch (Exception ex){
-            return new ResponseEntity<>("Hiba történt!", HttpStatus.BAD_REQUEST);
+            LOGGER.error("Error: ", ex);
+            return new ResponseEntity<>("There was an error!", HttpStatus.BAD_REQUEST);
         }
     }
 
     @RequestMapping(value = "/makeRecipe", method = RequestMethod.POST)
     public ResponseEntity<?> makeRecipe(@RequestBody Recipe recipe){
         try{
+            LOGGER.info("makeRecipe method called in controller");
             Recipe newRecipe = recipeService.makeRecipe(recipe);
             return new ResponseEntity<>(newRecipe, HttpStatus.CREATED);
         }
         catch (Exception ex){
-            return new ResponseEntity<>("Hiba történt!", HttpStatus.BAD_REQUEST);
+            LOGGER.error("Error: ", ex);
+            return new ResponseEntity<>("There was an error!", HttpStatus.BAD_REQUEST);
         }
     }
 
     @RequestMapping(value = "/getRecipe/{id}", method = RequestMethod.GET)
     public ResponseEntity<?> getRecipeById(@PathVariable Long id){
         try{
+            LOGGER.info("getRecipe method called in controller");
             Recipe recipe = recipeService.getRecipe(id);
             return new ResponseEntity<>(recipe, HttpStatus.OK);
         }
         catch (Exception ex){
-            return new ResponseEntity<>("Hiba történt!", HttpStatus.BAD_REQUEST);
+            LOGGER.error("Error: ", ex);
+            return new ResponseEntity<>("There was an error!", HttpStatus.BAD_REQUEST);
         }
     }
 
     @RequestMapping(value = "/changeRecipe", method = RequestMethod.PUT)
     public ResponseEntity<?> changeRecipe(@RequestBody Recipe recipe){
         try{
+            LOGGER.info("changeRecipe method called in controller");
             Recipe changedRecipe = recipeService.changeRecipe(recipe);
             return new ResponseEntity<>(changedRecipe, HttpStatus.ACCEPTED);
         }
         catch (Exception ex){
-            return new ResponseEntity<>("Hiba történt!", HttpStatus.BAD_REQUEST);
+            LOGGER.error("Error: ", ex);
+            return new ResponseEntity<>("There was an error!", HttpStatus.BAD_REQUEST);
         }
     }
 
     @RequestMapping(value = "/deleteRecipe/{id}", method = RequestMethod.DELETE)
     public ResponseEntity<?> deleteRecipe(@PathVariable Long id){
         try{
+            LOGGER.info("deleteRecipe method called in controller");
             recipeService.removeRecipe(id);
             return new ResponseEntity<>(HttpStatus.ACCEPTED);
         }
         catch (Exception ex){
-            return new ResponseEntity<>("Hiba történt!", HttpStatus.NOT_FOUND);
+            LOGGER.error("Error: ", ex);
+            return new ResponseEntity<>("There was an error!", HttpStatus.NOT_FOUND);
         }
     }
 }
--- src/main/java/com/portfolio/fullStackProject/service/RecipeServiceImpl.java
+++ src/main/java/com/portfolio/fullStackProject/service/RecipeServiceImpl.java
@@ -1,9 +1,12 @@
 package com.portfolio.fullStackProject.service;
 
+import com.portfolio.fullStackProject.controller.RecipeController;
 import com.portfolio.fullStackProject.entity.Recipe;
 import com.portfolio.fullStackProject.repository.RecipeRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -13,28 +16,36 @@ import java.util.List;
 public class RecipeServiceImpl implements RecipeService{
 
     private final RecipeRepository recipeRepository;
+
+    private static final Logger LOGGER = LogManager.getLogger(RecipeController.class);
+
     @Override
     public List<Recipe> getRecipes() {
+        LOGGER.info("getRecipes method called in service");
         return recipeRepository.findAll();
     }
 
     @Override
     public Recipe getRecipe(Long id) {
+        LOGGER.info("getRecipe method called in service");
         return recipeRepository.findById(id).orElseThrow(EntityNotFoundException::new);
     }
 
     @Override
     public Recipe makeRecipe(Recipe recipe) {
+        LOGGER.info("makeRecipe method called in service");
         return recipeRepository.save(recipe);
     }
 
     @Override
     public Recipe changeRecipe(Recipe recipe) {
+        LOGGER.info("changeRecipe method called in service");
         return recipeRepository.save(recipe);
     }
 
     @Override
     public void removeRecipe(Long id) {
+        LOGGER.info("removeRecipe method called in service");
         Recipe recipe = recipeRepository.findById(id)
                 .orElseThrow(EntityNotFoundException::new);
         recipeRepository.deleteById(id);
